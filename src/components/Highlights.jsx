6962e5ef35bde38bd0b9ead39c3d9e1d22cce986
Owen-San <iamnytla@gmail.com>
2025-06-04 19:55:06 -0400

    Initial commit

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Highlights() {
+  return (
+    <section id="highlights">
+        <div className="container">
+            <div className="row">
+                <h2 className="section__title">Why choose <span className="purple">Library</span></h2>
+            </div>
+        </div>
+    </section>
+  )
+}
