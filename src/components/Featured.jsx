bfc993211ab387e06cd6ca5efa332c17559efa9c
Owen-San <iamnytla@gmail.com>
2025-06-08 22:13:47 -0400

    Added books section

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,5 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import Book from "./ui/Book";
+import data from "../data";
 
 export default function Featured() {
   return (
@@ -10,33 +12,10 @@ export default function Featured() {
             Featured <span className="purple">Books</span>
           </h2>
           <div className="books">
-            <div className="book">
-              <a href="">
-                <figure className="book__img--wrapper">
-                  <img
-                    src="https://m.media-amazon.com/images/I/61mIq2iJUXL._AC_UF1000,1000_QL80_.jpg"
-                    alt=""
-                    className="book__img"
-                  />
-                </figure>
-              </a>
-              <div className="book__title">
-                <a href="/" className="book__title--link">
-                  Cracking the code
-                </a>
-              </div>
-              <div className="book__ratings">
-                <FontAwesomeIcon icon="star"/>
-                <FontAwesomeIcon icon="star"/>
-                <FontAwesomeIcon icon="star"/>
-                <FontAwesomeIcon icon="star"/>
-                <FontAwesomeIcon icon="star-half-alt"/>
-              </div>
-              <div className="book__price">
-                <span className="book__price--normal">$15.00</span>
-                $10.00
-              </div>
-            </div>
+            <Book />
+            <Book />
+            <Book />
+            <Book />
           </div>
         </div>
       </div>
