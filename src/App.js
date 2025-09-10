72f34ab18169d70b6eeb0bd457eba0062cbc4c66
Owen-San <iamnytla@gmail.com>
2025-09-10 00:03:01 -0400

    Added footer section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Discounted from "./components/Discounted";
 import Explore from "./components/Explore";
 import Featured from "./components/Featured";
+import { Footer } from "./components/Footer";
 import Highlights from "./components/Highlights";
 import Landing from "./components/Landing";
 import Nav from "./components/Nav";
@@ -15,6 +16,7 @@ function App() {
       <Featured />
       <Discounted />
       <Explore />
+      <Footer />
     </div>
   );
 }
