fff4b986aed9a5b5b273b49f9d66981221e1b447
Owen-San <iamnytla@gmail.com>
2025-09-10 00:19:28 -0400

    Fixed navigation menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,6 +3,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import LibraryLogo from "../assets/Library.svg";
 
 export default function Nav() {
+  function openMenu() {
+    document.body.classList += " menu--open";
+  }
+  function closeMenu() {
+    document.body.classList.remove ("menu--open");
+  }
+
   return (
     <nav>
       <div className="nav__container">
@@ -16,11 +23,11 @@ export default function Nav() {
             </a>
           </li>
           <li className="nav__list">
-            <a href="/" className="nav__link">
+            <a href="/books" className="nav__link">
               Books
             </a>
           </li>
-          <button className="btn__menu">
+          <button className="btn__menu" onClick={openMenu}>
             <FontAwesomeIcon icon="bars" />
           </button>
           <li className="nav__icon">
@@ -31,7 +38,7 @@ export default function Nav() {
           </li>
         </ul>
         <div className="menu__backdrop">
-          <button className="btn__menu btn__menu--close">
+          <button className="btn__menu btn__menu--close" onClick={closeMenu}>
             <FontAwesomeIcon icon="times" />
           </button>
           <ul className="menu__links">
