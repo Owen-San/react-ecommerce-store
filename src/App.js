7b3c28dd8f01d7b9af2db105f36898af4e15d071
Owen-San <iamnytla@gmail.com>
2025-09-11 19:59:37 -0400

    Added book info page with dynamic elements. Also added new components.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 import Discounted from "./components/Discounted";
 import Explore from "./components/Explore";
 import Featured from "./components/Featured";
@@ -5,21 +7,33 @@ import { Footer } from "./components/Footer";
 import Highlights from "./components/Highlights";
 import Landing from "./components/Landing";
 import Nav from "./components/Nav";
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import "./index.css";
+
 import { Home } from "./pages/Home";
 import { Books } from "./pages/Books";
 import { books } from "./data";
+import { BookInfo } from "./pages/BookInfo";
 
 function App() {
-  return(
+  return (
     <Router>
-    <div className="App">
-      <Nav />
-      <Route path="/" exact component={Home} />
-      <Route path="/books" render={() => <Books books={books} />}/>
-      <Footer />
-    </div>
+      <div className="App">
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" exact element={<Books books={books} />} />
+          <Route path="/books/:id" element={<BookInfo books={books} />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   );
 }
